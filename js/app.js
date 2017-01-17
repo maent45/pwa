52f96ba81545382fb949441a7d219cac3874446d
Maen Terawasi <maent45@gmail.com>
2017-01-17 22:49:11 +1300

    using news api

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$.getJSON("https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=f639fcbf28744c87a2a59c86cfc28a14", function(data){
+
+  console.log(data);
+
+  for (var i = 0; i < 10; i++) {
+
+    $(".article-list").append(
+      "<li>"
+      + "<h3>" + data.articles[i].title + "</h3>"
+      + "<img src=" + data.articles[i].urlToImage + ">"
+      + "<i>Author: " + data.articles[i].author + "</i>"
+      + "<p>" + data.articles[i].description + "</p>"
+      + "</li>"
+
+    );
+
+  }
+
+});
+
+$("a").on('click', function() {
+  console.log(this.attr("href"));
+});
