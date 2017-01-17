6ad5457a45834db0dc4f2a491c34c665a26e599c
Maen Terawasi <maent45@gmail.com>
2017-01-17 23:53:34 +1300

    offline caching done

--- service-worker.js
+++ service-worker.js
@@ -38,5 +38,46 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  // console.log('[ServiceWorker] fetching', e.request.url);
+  console.log('[ServiceWorker] fetching', e.request.url);
+
+  // e.respondWidth Responds to the fetch event
+  e.respondWith(
+
+    // Check in cache for the request being made
+    caches.match(e.request).then(function(response) {
+
+      // If the request is in the cache
+      if (response) {
+        console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+        // Return the cached version
+        return response;
+      }
+
+      // If the request is NOT in the cache, fetch and cache
+      var requestClone = e.request.clone();
+      fetch(requestClone)
+        .then(function(response) {
+
+          if (!response) {
+            console.log('[ServiceWorker] no response from fetch');
+            return response;
+          }
+
+          var responseClone = response.clone();
+
+          caches.open(cacheName).then(function(cache) {
+            cache.put(e.request, responseClone);
+            return response;
+          });
+
+
+        })
+        .catch(function(err) {
+          console.log('[ServiceWorker] error fetching', err);
+        })
+
+    })
+
+  )
+
 });
