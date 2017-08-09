3d931a2531cc893e9d76083c18f50d0ea6e8c7c8
maent <maen.terawasi@acc.co.nz>
2017-08-10 10:14:45 +1200

    initial commit - adding Ionic project

---
+++ src/pages/list/list.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+
+import { NavController, NavParams } from 'ionic-angular';
+
+import { ItemDetailsPage } from '../item-details/item-details';
+
+@Component({
+  selector: 'page-list',
+  templateUrl: 'list.html'
+})
+
+export class ListPage {
+  icons: string[];
+  items: Array<{title: string, note: string, icon: string}>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
+    'american-football', 'boat', 'bluetooth', 'build'];
+    
+    this.items = [
+      {
+        title:'first',
+        note:'first note',
+        icon:'flask'
+      },
+      {
+        title:'second',
+        note:'second note',
+        icon:'wifi'
+      },
+      {
+        title:'third',
+        note:'third note',
+        icon:'beer'
+      }
+    ];
+    for(let i = 1; i < 3; i++) {
+      // this.items.push({
+        // title: 'Item ' + i,
+        // note: 'This is item #' + i,
+        // icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+      // });
+      this.items;
+    }
+  }
+
+  itemTapped(event, item) {
+    this.navCtrl.push(ItemDetailsPage, {
+      item: item
+    });
+  }
+}
