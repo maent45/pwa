65dd9ee943d401e766f8a32e063f809ea686204f
maent45 <maent45@gmail.com>
2017-08-10 10:24:28 +1200

    starting Register page

---
+++ src/pages/register/register.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-register',
+    templateUrl: 'register.html'
+})
+
+export class RegisterPage() {
+    
+    constructor() {
+        
+        
+        
+    }
+    
+}
